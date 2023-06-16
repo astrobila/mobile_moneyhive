df994476068f3ae42766836798409158700a5173
astrobila <astro.bila@gmail.com>
2023-06-16 19:19:12 +0700

    .

--- lib/libraries/models/transactions_model.dart
+++ lib/libraries/models/transactions_model.dart
@@ -1,17 +1,28 @@
 import 'dart:convert';
+import 'dart:ffi';
 import 'package:dio/dio.dart';
 
 import 'package:moneyhive/app_config.dart';
 import 'package:moneyhive/libraries/diox.dart';
 import 'package:moneyhive/libraries/session.dart';
 
-class Transaction {}
+class Transaction {
+  int? id;
+  int? userId;
+  String? title;
+  String? description;
+  int? status;
+  double? itemPrice;
+  double? progress;
+}
 
 class TransactionModel {
   static const _listURL = '${AppConfig.baseURL}/api.transactions.php';
   static const _addURL = '${AppConfig.baseURL}/api.transactions.add.php';
   static const _editURL = '${AppConfig.baseURL}/api.transactions.edit.php';
   static const _deleteURL = '${AppConfig.baseURL}/api.transactions.delete.php';
+  static const _balanceURL =
+      '${AppConfig.baseURL}/api.transactions.balance.php';
 
   static Future<List<Transaction>> getList() async {
     SessionData sessData = await Session.getURL(
@@ -21,5 +32,20 @@ class TransactionModel {
     List<Transaction> Transactions = [];
 
     return Transactions;
+  }
+
+  static Future<double> getBalance() async {
+    SessionData sessData = await Session.getURL(
+        '$_balanceURL?${DateTime.now().millisecondsSinceEpoch.toString()}');
+    double Balance = 0.2;
+    return Balance;
+  }
+
+  static Future<int> delete(Transaction transaction) async {
+    SessionData sessData = await Session.postURL(
+        '$_deleteURL?${DateTime.now().millisecondsSinceEpoch.toString()}', {
+      'id': transaction.id,
+    });
+    return sessData.status;
   }
 }
--- lib/libraries/models/wishlist_model.dart
+++ lib/libraries/models/wishlist_model.dart
@@ -5,6 +5,74 @@ import 'package:moneyhive/app_config.dart';
 import 'package:moneyhive/libraries/diox.dart';
 import 'package:moneyhive/libraries/session.dart';
 
-class Wishlist {}
+class Wishlist {
+  int? id;
+  int? userId;
+  String? title;
+  String? description;
+  int? status;
+  double? itemPrice;
+  double? progress;
 
-class WishlistModel {}
+  Wishlist(
+      {this.id,
+      this.userId,
+      this.title,
+      this.description,
+      this.status,
+      this.itemPrice,
+      this.progress});
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'userId': userId,
+      'title': title,
+      'description': description,
+      'status': status,
+      'itemPrice': itemPrice,
+      'progress': progress,
+    };
+  }
+
+  factory Wishlist.fromMap(Map<String, dynamic> map) {
+    return Wishlist(
+      id: map['id'] as int,
+      userId: map['userId'] as int,
+      title: (map['title'] ?? '') as String,
+    );
+  }
+}
+
+class WishlistModel {
+  static const _listURL = '${AppConfig.baseURL}/api.wishlist.php';
+  static const _addURL = '${AppConfig.baseURL}/api.wishlist.add.php';
+  static const _editURL = '${AppConfig.baseURL}/api.wishlist.edit.php';
+  static const _deleteURL = '${AppConfig.baseURL}/api.wishlist.delete.php';
+
+  static Future<List<Wishlist>> getList() async {
+    SessionData sessData = await Session.getURL(
+        '$_listURL?${DateTime.now().millisecondsSinceEpoch.toString()}');
+    if (sessData.status != 1) return [];
+
+    List<Wishlist> Wishlists = [];
+
+    return Wishlists;
+  }
+
+  static Future<int> add(Wishlist wishlist) async {
+    SessionData sessData = await Session.postURL(
+        '$_addURL?${DateTime.now().millisecondsSinceEpoch.toString()}', {
+      'title': wishlist.title,
+    });
+    return sessData.status;
+  }
+
+  static Future<int> delete(Wishlist wishlist) async {
+    SessionData sessData = await Session.postURL(
+        '$_deleteURL?${DateTime.now().millisecondsSinceEpoch.toString()}', {
+      'id': wishlist.id,
+    });
+    return sessData.status;
+  }
+}
